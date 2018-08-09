ba82b602357310d2e5a1128bfa20720ec485d9a7
flaminestone <flamine@list.ru>
2018-08-09 18:13:45 +0300

    Split spec for two because need to use short and informative spec and describe names

---
+++ spec/check_statuses_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe OnlyofficeTcmHelper do
+  describe 'Check comments' do
+    tcm_helper = OnlyofficeTcmHelper::TcmHelper.new(product_name: 'Product',
+                                                    plan_name: 'Plan',
+                                                    suite_name: description)
+
+
+    it 'check comment for passed status' do
+      expect(tcm_helper.parse(PseudoExamplePassed.new('check comment for passed status')).comment).to eq("\nOk")
+    end
+
+    it 'check comment for passed_2 status' do
+      tcm_helper.parse(PseudoExamplePassed.new('check comment for passed_2 status'))
+      expect(tcm_helper.parse(PseudoExamplePassed.new('check comment for passed_2 status')).comment).to eq("\nPassed 2")
+    end
+
+    it 'check comment for failed status' do
+      expect(tcm_helper.parse(PseudoExampleFailed.new('check comment for passed_2 status')).comment).not_to be_empty
+    end
+
+    it 'check comment for aborted status' do
+      expect(tcm_helper.parse(PseudoExampleAborted.new('check comment for aborted status')).comment).not_to be_empty
+    end
+
+    it 'check comment for pending status' do
+      expect(tcm_helper.parse(PseudoExamplePending.new('check comment for pending status')).comment).to eq('Pending exception')
+    end
+
+    it 'check comment for service_unavailable status' do
+      expect(tcm_helper.parse(PseudoExampleServiceUnavailable.new('check comment for service_unavailable status')).comment).to eq("\nService Unavailable: 503")
+    end
+
+    it 'check comment for lpv status' do
+      expect(tcm_helper.parse(PseudoExampleLPV.new('check comment for lpv status')).comment).to eq("\nLimited program version")
+    end
+  end
+end
